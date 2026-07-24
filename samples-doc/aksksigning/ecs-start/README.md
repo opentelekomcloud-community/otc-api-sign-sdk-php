387d440be8b58c106cba89d4412c03052de7b106
Harald Sack <harald.sack@t-systems.com>
2026-07-24 12:03:29 +0200

    sample added

---
+++ samples-doc/aksksigning/ecs-start/README.md
@@ -0,0 +1,25 @@
+# Start ECS instance using AK/SK signing
+
+## Environment variables
+
+| Env variable      | Value
+| ----------------- | -----------------
+| OTC_SDK_PROJECT_ID| Project ID
+| ECS_INSTANCE_ID   | ECS Instance ID
+| OTC_SDK_AK        | Access Key
+| OTC_SDK_SK        | Secret Key
+
+
+## How to run
+
+Install dependencies
+
+```
+composer install
+```
+
+Execute php program
+
+```
+php startECS.php
+```
---
+++ samples-doc/aksksigning/ecs-start/startECS.php
@@ -0,0 +1,65 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+function startECS() {
+    $project_id = getenv('OTC_SDK_PROJECT_ID');
+    $endpoint = 'ecs.eu-de.otc.t-systems.com';
+    $instance_id = getenv('ECS_INSTANCE_ID');
+
+    $signer = new Signer();
+    $signer->Key = getenv('OTC_SDK_AK');
+    $signer->Secret = getenv('OTC_SDK_SK');
+
+    echo "Starting ECS instance: " . $instance_id . "\n";
+
+    // see https://docs.otc.t-systems.com/elastic-cloud-server/api-ref/apis_recommended/batch_operations/starting_ecss_in_a_batch.html#en-us-topic-0020212207
+    $url = 'https://' . $endpoint . '/v1/' . $project_id . '/cloudservers/action';
+
+    $body = json_encode([
+        'os-start' => [
+            'servers' => [['id' => $instance_id]],
+        ],
+    ]);
+
+    $headers = [
+        'Content-Type' => 'application/json;charset=utf8',
+    ];
+
+    if ($project_id !== false && $project_id !== '') {
+        // To access resources in a sub-project (e.g. eu_de/myproject)
+        // by calling APIs, X-Project-Id of "eu_de/myproject" is needed
+        $headers['X-Project-Id'] = $project_id;
+    }
+
+    $req = new Request('POST', $url, $headers, $body);
+
+    $curl = $signer->Sign($req);
+
+    // use proxy if defined in environment
+    $proxy = getenv('HTTP_PROXY');
+    if ($proxy !== false && $proxy !== '') {
+        echo "Using HTTP proxy: " . $proxy . "\n";
+        curl_setopt($curl, CURLOPT_PROXY, $proxy);
+    }
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HEADER, true);
+
+    $response = curl_exec($curl);
+    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+
+    if ($status == 0) {
+        echo "Error: " . curl_error($curl) . "\n";
+    } else {
+        $response_headers = substr($response, 0, $header_size);
+        $response_body = substr($response, $header_size);
+        echo "Status Code: " . $status . "\n";
+        echo "Headers: " . $response_headers . "\n";
+        echo "Response Body: " . $response_body . "\n";
+    }
+
+    curl_close($curl);
+}
+
+startECS();
